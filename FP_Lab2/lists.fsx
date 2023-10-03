c0ad630ea046fbdb58a0eb15a37a357bc528b668
lkhorasandzhian <levon.kh228@gmail.com>
2023-10-03 19:55:06 +0300

    full completed task 1 was uploaded

--- FP_Lab2/lists.fsx
+++ FP_Lab2/lists.fsx
@@ -7,6 +7,44 @@ Khorasandzhian Levon, group 218.
 // return a grouped list,
 // eg. [(1,4); (1,6); (2,4); (4,2); (4,3)] -> [(1, [4; 6]); (2, [4]); (4, [2; 3])]
 
+// Recoursion.
+let groupPairsByFirstElement1 =
+    let rec groupPairs currentGroup = function
+     | [] -> [currentGroup]
+     | (x, y)::restPairs when x = fst currentGroup ->
+         groupPairs (x, snd currentGroup@[y]) restPairs
+     | (x, y)::restPairs ->
+         currentGroup::groupPairs (x, [y]) restPairs
+
+    function
+     | [] -> []
+     | (x, y)::restPairs -> groupPairs (x, [y]) restPairs
+
+groupPairsByFirstElement1 [(1,4); (1,6); (2,4); (4,2); (4,3)]
+
+// Tail recoursion.
+let groupPairsByFirstElement2 =
+    let rec groupPairs acc currentGroup = function
+     | [] -> acc@[currentGroup]
+     | (x, y)::restPairs when x = fst currentGroup ->
+         groupPairs acc (x, snd currentGroup@[y]) restPairs
+     | (x, y)::restPairs ->
+         groupPairs (acc@[currentGroup]) (x, [y]) restPairs
+
+    function
+     | [] -> []
+     | (x, y)::restPairs -> groupPairs [] (x, [y]) restPairs
+
+groupPairsByFirstElement2 [(1,4); (1,6); (2,4); (4,2); (4,3)]
+
+// Library functions.
+let groupPairsByFirstElement3 L =
+    List.groupBy(fun (x, _) -> x) L |>
+    List.map(fun (x, L') -> (x, snd(List.unzip L')))
+
+groupPairsByFirstElement3 [(1,4); (1,6); (2,4); (4,2); (4,3)]
+
+
 // Task 2 - variant 1.
 // Compute the sum of positive elements of a list.
 
@@ -38,6 +76,7 @@ SumOfPositiveElems2 [-7; 9; -2; 3; 8; -6; -10; 4; -1; -4; 7; -9; 10; -3; -8; 5; 
 let SumOfPositiveElems3 L = List.filter((<) 0) L |> List.sum
 
 SumOfPositiveElems3 [-7; 9; -2; 3; 8; -6; -10; 4; -1; -4; 7; -9; 10; -3; -8; 5; -5; 6; -7; 2]
+
 
 // Task 3 - variant 4.
 // You need to implement ALL of the following queries:
